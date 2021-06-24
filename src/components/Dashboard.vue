858ac158814fde39ca9c64c0dabdab7cfe850f91
zackcarlson014 <jzcarlson014@gmail.com>
2021-06-23 20:50:06 -0500

    dashboard and muscle group components created

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>
+      {{title}}
+    </h1>
+    <h3>
+      {{keyToSuccess}}
+    </h3>
+    <MuscleGroup v-for="(muscle, i) in muscleGroups" :key="i" :muscle="muscle"/>
+  </div>
+</template>
+
+<script>
+
+import MuscleGroup from './MuscleGroup.vue'
+
+export default {
+  name: "Dashboard",
+  components: {
+    MuscleGroup
+  },
+  data: () => ({
+    title: 'Ten Thousand Reps',
+    keyToSuccess: 'You gotta want it!',
+    muscleGroups: [
+      'Legs',
+      'Back',
+      'Shoulders',
+      'Chest',
+      'Core',
+      'Arms',
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MuscleGroup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h5>
+      {{muscle}}
+    </h5>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    muscle: String
+  }
+}
+</script>
+
+<style>
+
+</style>
