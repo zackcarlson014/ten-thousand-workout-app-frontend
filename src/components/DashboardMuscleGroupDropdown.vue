7da916c35c33318081b20c95e156f4c5d312451a
zackcarlson014 <jzcarlson014@gmail.com>
2021-07-01 19:23:11 -0500

    styling updates muscle groups/calendar

--- src/components/DashboardMuscleGroupDropdown.vue
+++ src/components/DashboardMuscleGroupDropdown.vue
@@ -1,27 +1,27 @@
 <template>
-    <v-expansion-panel>
-      <v-expansion-panel-header expand-icon="mdi-menu">
-        <h1>{{muscle}}</h1>
-      </v-expansion-panel-header>
-      <v-expansion-panel-content>
-        <v-container>
-          <v-row justify="center">
-            <v-col 
-              v-for="(exercise, i) in exercises" 
-              :key="i" 
-              cols="12" 
-              sm="3"
-            >
-              <v-chip-group>
-                <v-row justify="center">
-                  <DashboardExerciseTile :exercise="exercise"/>
-                </v-row>
-              </v-chip-group>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
+  <v-expansion-panel>
+    <v-expansion-panel-header expand-icon="mdi-menu">
+      <h1>{{muscle}}</h1>
+    </v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <v-container>
+        <v-row justify="center">
+          <v-col 
+            v-for="(exercise, i) in exercises" 
+            :key="i" 
+            cols="12" 
+            sm="3"
+          >
+            <v-chip-group>
+              <v-row justify="center">
+                <DashboardExerciseTile :exercise="exercise"/>
+              </v-row>
+            </v-chip-group>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
 </template>
 
 <script>
--- src/components/MuscleGroupsViewExerciseCard.vue
+++ src/components/MuscleGroupsViewExerciseCard.vue
@@ -1,58 +1,26 @@
 <template>
-  <v-card
-    elevation="24"
-    max-width="444"
-    class="mx-auto"
-  >
-    <v-system-bar lights-out></v-system-bar>
-    <v-carousel
-      :continuous="false"
-      :cycle="cycle"
-      :show-arrows="true"
-      hide-delimiter-background
-      delimiter-icon="mdi-minus"
-      height="300"
+  <v-carousel v-model="model">
+    <v-carousel-item
+      v-for="(color, i) in colors"
+      :key="color"
     >
-      <v-carousel-item
-        v-for="(slide, i) in slides"
-        :key="i"
+      <v-sheet
+        :color="color"
+        height="100%"
+        tile
       >
-        <v-sheet
-              class="d-flex"
-              color="red lighten-3"
-              height="250"
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
         >
-          <v-row
-            class="fill-height"
-            align="center"
-            justify="center"
-          >
-            <div class="text-h2">
-              {{ slide }} Slide
-            </div>
-          </v-row>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
-    <v-list two-line>
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>John Leider</v-list-item-title>
-          <v-list-item-subtitle>Author</v-list-item-subtitle>
-        </v-list-item-content>
-        <v-list-item-action>
-          <v-switch
-            v-model="cycle"
-            label="Cycle Slides"
-            inset
-          ></v-switch>
-        </v-list-item-action>
-      </v-list-item>
-    </v-list>
-  </v-card>
+          <div class="text-h2">
+            Slide {{ i + 1 }}
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
